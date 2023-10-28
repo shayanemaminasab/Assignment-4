00cdc690e0779e28313d9f9717c55cf8857ab6cf
Shayan Emaminasab <148277606+shayanemaminasab@users.noreply.github.com>
2023-10-28 22:49:03 +0330

    Add files via upload

---
+++ Exercise 2.py
@@ -0,0 +1,14 @@
+import random
+
+while True:
+    dice = random.randint(0,6)
+
+    while dice == 6:
+        print("Your dice is 6")
+        print("You can walk 6 steps ahead and roll the dice again!")
+        dice = random.randint(0,6)
+
+    if dice == 1 or dice == 2 or dice == 3 or dice == 4 or dice == 5:
+        print("Your dice is", dice)
+        print("You can walk", dice,"steps ahead!")
+        break

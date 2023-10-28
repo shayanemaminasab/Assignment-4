2ac9490fa5c9a1eead9c6ae060cf3793ffc3b6ab
Shayan Emaminasab <148277606+shayanemaminasab@users.noreply.github.com>
2023-10-28 22:52:45 +0330

    Add files via upload

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,12 +1,12 @@
 import random
 
 while True:
-    dice = random.randint(0,6)
+    dice = random.randint(1,6)
 
     while dice == 6:
         print("Your dice is 6")
         print("You can walk 6 steps ahead and roll the dice again!")
-        dice = random.randint(0,6)
+        dice = random.randint(1,6)
 
     if dice == 1 or dice == 2 or dice == 3 or dice == 4 or dice == 5:
         print("Your dice is", dice)
